781a36aa91422a3f292af0a8cca844cb0ce8aba7
Dulminashehan <dulminashehan596@gmail.com>
2026-03-15 00:43:55 +0530

    add flame sensor

--- Code/esp32/esp32.ino
+++ Code/esp32/esp32.ino
@@ -3,7 +3,9 @@
 
 #define LED_PIN 2      // Built-in LED
 #define DHTPIN 4       // DHT sensor DATA pin
-#define DHTTYPE DHT22  // Change to DHT11 if using that
+#define DHTTYPE DHT22  // DHT22 or DHT11
+
+#define FLAME_PIN 15   // Connect D1 to GPIO15
 
 const char* ssid = "Dulmina";
 const char* password = "dula1790";
@@ -13,8 +15,9 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(FLAME_PIN, INPUT);
 
-  // WiFi
+  // WiFi connection
   Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -26,7 +29,7 @@ void setup() {
   Serial.print("ESP32 IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // DHT sensor
+  // Start DHT sensor
   dht.begin();
 }
 
@@ -37,18 +40,27 @@ void loop() {
   digitalWrite(LED_PIN, LOW);
   delay(500);
 
-  // Read temperature & humidity
+  // Read DHT22
   float tempC = dht.readTemperature();
   float hum = dht.readHumidity();
-
   if (isnan(tempC) || isnan(hum)) {
     Serial.println("Failed to read from DHT sensor!");
-    return;
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print(" °C, Humidity: ");
+    Serial.print(hum);
+    Serial.println(" %");
   }
 
-  Serial.print("Temperature: ");
-  Serial.print(tempC);
-  Serial.print(" °C, Humidity: ");
-  Serial.print(hum);
-  Serial.println(" %");
+  // Read Flame sensor
+  int flameState = digitalRead(FLAME_PIN);
+  if (flameState == LOW) { // LOW = flame detected (depends on module)
+    Serial.println("🔥 Flame Detected!");
+    digitalWrite(LED_PIN, HIGH); // Turn LED ON
+  } else {
+    Serial.println("No Flame");
+  }
+
+  delay(1000);
 }
